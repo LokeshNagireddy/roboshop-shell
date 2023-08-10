cb3ad9ee332080fd3f76b513adba4f3ec9a4220f
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2023-08-10 20:32:09 +0530

    rohoshop-shell changes

--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,22 @@
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
+mysql_root_password=$1
+
+if [ -z "$mysql_root_password" ]; then
+  echo Input MySQL Root Password Missing
+  exit 1
+fi
+
 echo -e "\e[35m>>>>>>>>> Disable Existing mysql version  <<<<<<<<\e[0m"
 dnf module disable mysql -y
 echo -e "\e[35m>>>>>>>>> Create mysql repo file  <<<<<<<<\e[0m"
-cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+cp ${script_path}/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
 echo -e "\e[35m>>>>>>>>> install mysql  <<<<<<<<\e[0m"
 yum install mysql-community-server -y
 echo -e "\e[35m>>>>>>>>> Enable & Start mysql service  <<<<<<<<\e[0m"
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
 echo -e "\e[35m>>>>>>>>> Update credentials  <<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
-mysql -uroot -pRoboShop@1
+mysql_secure_installation --set-root-pass $mysql_root_password
+#mysql -uroot -pRoboShop@1
