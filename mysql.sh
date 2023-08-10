947affcb83fc4b954537a8cefdd97f8eeedda410
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2023-08-10 21:06:29 +0530

    rohoshop-shell changes

--- mysql.sh
+++ mysql.sh
@@ -11,7 +11,7 @@ fi
 echo -e "\e[35m>>>>>>>>> Disable Existing mysql version  <<<<<<<<\e[0m"
 dnf module disable mysql -y
 echo -e "\e[35m>>>>>>>>> Create mysql repo file  <<<<<<<<\e[0m"
-cp ${script_path}/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo
 echo -e "\e[35m>>>>>>>>> install mysql  <<<<<<<<\e[0m"
 yum install mysql-community-server -y
 echo -e "\e[35m>>>>>>>>> Enable & Start mysql service  <<<<<<<<\e[0m"
